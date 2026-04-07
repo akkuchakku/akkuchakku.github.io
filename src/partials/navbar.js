403f35e3d7a4c37759f31bb344542131546af8ff
akkuchakku <125223294+akkuchakku@users.noreply.github.com>
2026-04-07 16:34:28 +1000

    navbar.js

--- src/partials/navbar.js
+++ src/partials/navbar.js
@@ -6,12 +6,12 @@ class Nav extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <head>
-                <link rel="stylesheet" href="super.css">
+                <link rel="stylesheet" href="/src/assets/css/super.css">
             </head>
             <div class="nav-main">
                 <div class="container">
                     <nav class="navbar navbar-expand-lg navbar-light">
-                        <a class="navbar-brand font-weight-bold" href="index.html">Portfolio</a>
+                        <a class="navbar-brand font-weight-bold" href="/index.html">Portfolio</a>
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
