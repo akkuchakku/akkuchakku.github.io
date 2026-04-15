832df3939146cb5e36a8d3857f760e723d77eb58
akkuchakku <125223294+akkuchakku@users.noreply.github.com>
2026-04-15 15:40:00 +1000

    whole repo/fixed symlinks and added navbar to all files

--- src/partials/footer.js
+++ src/partials/footer.js
@@ -6,7 +6,7 @@ class Footer extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <head>
-                <link rel="stylesheet" href="/assets/css/super.css">
+                <link rel="stylesheet" href="/assets/css/assets/css/super.css">
             </head>
             <footer class="position-relative">
                 <div class="container">
